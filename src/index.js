bc1a69c0df09d14d717b1ff4083274eff10ad6e9
Dinei <dinei@pop-os.localdomain>
2021-09-05 21:39:19 -0300

    initial modifications

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
+/**
+ * GET - BUSCAR UMA INFORMAÇÃO DENTRO DO SERVIDOR
+ * POST - INSERIR UMA INFORMAÇÃO NO SERTVIDOR
+ * PUT - ALETAR UMA INFORMAÇÃO NO SERTVIDOR
+ * PATCH - ALTERAR UMA INFORMAÇÃO ESPECIFICA
+ * DELETE - DELETAR UMA INFORMAÇÃO NO SERVIDOR
+ */
 
-app.get("/" , (request, response) => {
-  return response.json({ message: "Hello World Ignite - Fundamentos NoedJS!" });
+app.get("/courses" , (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1" , "Curso 2" , "Curso 3" , "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6" , "Curso 2" , "Curso 3" , "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6" , "Curso 7" , "Curso 3" , "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6" , "Curso 2" , "Curso 4"]);
 });
 
 app.listen(3333);
